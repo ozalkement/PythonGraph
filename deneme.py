bd69cf4737cead58b965d1e7202f44a79875ed71
ozalkement <ozalkement@gmail.com>
2020-11-22 15:35:47 +0300

    Deneme Sayfası Silme işlemi
    Bazı satırlar silindi

--- deneme.py
+++ deneme.py
@@ -1,12 +1,3 @@
 import numpy as np
 import pandas as pd
-import matplotlib.oyplot as plt
-import seaborn as sb
-import numpy as np
-import pandas as pd
-import matplotlib.oyplot as plt
-import seaborn as sb
-import numpy as np
-import pandas as pd
-import matplotlib.oyplot as plt
-import seaborn as sb
+
